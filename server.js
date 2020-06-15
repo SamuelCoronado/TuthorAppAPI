cfa7c1f5cc8414dc7379141a8a3177643569e48a
Samuel <coronado_samuel@hotmail.com>
2020-06-15 15:18:37 -0500

    prueba CORS

--- server.js
+++ server.js
@@ -8,10 +8,21 @@ const path = require('path')
 //Connect database
 connectDB();
 
+
 //Init middleware
-app.use(express.json({extended: false}))
-app.use(cors())
-app.use(morgan('dev'))
+app.use(express.json({extended: false}));
+app.use(morgan('dev'));
+//app.use(cors());
+
+// Configurar cabeceras y cors
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
 
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
